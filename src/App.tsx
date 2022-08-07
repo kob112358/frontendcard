c9071f364752eebf932a346cbd2fe3c8795adddc
kob112358 <kob112358@gmail.com>
2022-08-06 19:19:46 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  0000 0000 0000 0000
+  Jane Appleseed
+  00/00
+
+  000
+
+  Cardholder Name
+  e.g. Jane Appleseed
+
+  Card Number
+  e.g. 1234 5678 9123 0000
+
+  Exp. Date (MM/YY)
+  MM
+  YY
+
+  CVC
+  e.g. 123
+
+  Confirm
+
+  <p>- Completed state start -</p>
+
+  Thank you!
+  We've added your card details
+  Continue
+  
+  <div className="attribution">
+    Challenge by <a href="https://www.frontendmentor.io/challenges/interactive-card-details-form-XpS8cKZDWw">Frontend Mentor</a>. 
+    Coded by <a href="https://www.frontendmentor.io/profile/kob112358">Eric Kobliska</a>.
+  </div>
+    </div>
+  );
+}
+
+export default App;
